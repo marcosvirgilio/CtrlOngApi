d06ef0753bf3c474cf35a304c103c055577ee0a2
Marcos Virgilio <57771135+marcosvirgilio@users.noreply.github.com>
2025-10-23 16:48:57 -0300

    v1.0

---
+++ conexao.php
@@ -0,0 +1,18 @@
+
+<?php
+
+//ambiente marcosvirgilio.online
+$servername = '127.0.0.1';
+$username = 'root';
+$password = 'aluno';
+$dbname = 'ong';
+
+// Create connection
+$con = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
+
+?>
